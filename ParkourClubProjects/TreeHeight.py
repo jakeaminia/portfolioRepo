34ba874d43924da30795b8c88b86f5e1f001a1b2
jakeaminia <jake.aminia@gmail.com>
2023-04-19 13:49:55 -0500

    began implementing object-oriented version of program

--- ParkourClubProjects/TreeHeight.py
+++ ParkourClubProjects/TreeHeight.py
@@ -1,3 +1,51 @@
+class node:
+    def __init__(self, name, prerequisites):
+        self._name = name
+        self._direct_prerequisites = set(prerequisites)
+        self._extended_prerequisites = set()
+        self._height = None
+        self._count_direct_prerequisites = None
+
+    def add_direct_prerequisite(self, prerequisite):
+        self._direct_prerequisites.append(prerequisite)
+
+    def get_direct_prerequisites(self):
+        return self._direct_prerequisites
+
+    def get_extended_prerequisites(self):
+        return self._extended_prerequisites
+
+    def set_name(self, new_name):
+        self._name = new_name
+
+    def get_name(self):
+        return self._name
+
+    def get_height(self):
+        return self._height
+
+    def set_height(self):
+        print('re-calculate extended prerequisites')
+        print('re-calculate height')
+        for prerequisite in self._direct_prerequisites:
+            self._extended_prerequisites.add(prerequisite)
+            prerequisite.set
+
+    def set_count_direct_prerequisites(self):
+        self._count_direct_prerequisites = len(self._direct_prerequisites)
+    
+    def get_count_direct_prerequisites(self):
+        return self._count_direct_prerequisites
+
+    def __str__(self):
+        formatted_direct_prerequisites = ''
+        for index, prerequisite in enumerate(self._direct_prerequisites):
+            formatted_direct_prerequisites += f'{prerequisite}'
+            if index < self._count_direct_prerequisites:
+                formatted_direct_prerequisites += ', '
+        result = f'{self._name} ({formatted_direct_prerequisites})'
+
+
 # Input file is loosely formatted because it was derived from a copy-and-paste of a google doc table
 def parse_google_doc_table(doc_name: str):
     matrix = dict()
@@ -30,6 +78,8 @@ def get_all_prerequisites(node: str, matrix: dict) -> set:
     return result
 
 # Defining "node height" as the total number of prerequisites for a given node
+
+
 def get_heights(matrix: dict):
     node_heights_dict = dict()
     sorted_nodes = list()
@@ -46,28 +96,27 @@ def get_heights(matrix: dict):
 
 def print_matrix(matrix: dict):
     with open('result_doc.txt', 'w') as result_doc:
-        nodes, node_heights = get_heights(matrix)
-        requisite_map = dict()
-        for i, value in enumerate(nodes):
-            num_prerequisites = node_heights[i]
-            if num_prerequisites not in requisite_map:
-                requisite_map[num_prerequisites] = list()
-            requisite_map[num_prerequisites].append((value, matrix.get(value)))
-        for index, value in enumerate(sorted(requisite_map.keys())):
-            items = requisite_map.get(value)
-            # print(f'Level {index}:\n')
+        sorted_nodes, sorted_nodes_heights = get_heights(matrix)
+        # node_height_levels maps height numbers to a list of (node, [node prerequisites]) for nodes of that height in sorted_nodes
+        node_height_levels = dict()
+        for i, value in enumerate(sorted_nodes):
+            num_prerequisites = sorted_nodes_heights[i]
+            if num_prerequisites not in node_height_levels:
+                node_height_levels[num_prerequisites] = list()
+            node_height_levels[num_prerequisites].append(
+                (value, matrix.get(value)))
+        for index, value in enumerate(sorted(node_height_levels.keys())):
+            items = node_height_levels.get(value)
             result_doc.write(f'Level {index}:\n\n')
             for item in items:
                 pre_str = ''
                 if not item[1]:
-                    pre_str += 'None1'
+                    pre_str += 'None.'
                 else:
                     for pre in item[1]:
                         pre_str += f'{pre}, '
-                # print(f'\t{item[0]} ({pre_str.strip()[0:-1]})')
                 result_doc.write(f'\t{item[0]} ({pre_str.strip()[0:-1]})\n')
 
-            # print('')
             result_doc.write('\n')
 
 
