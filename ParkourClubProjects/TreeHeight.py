85a8c0f617c46f29ce2149f873acc11d6e27ca25
jakeaminia <jake.aminia@gmail.com>
2023-04-20 17:06:41 -0500

    Finished development of object-oriented version except for printing to file

--- ParkourClubProjects/TreeHeight.py
+++ ParkourClubProjects/TreeHeight.py
@@ -1,122 +1,5 @@
 from operator import methodcaller
 
-class DAG:
-    def __init__(self, name: str):
-        self._node_dict = dict()
-        self._name = name
-        self._sorted_nodes = list()
-    def add(self, node):
-        self._node_dict[node.get_name()] = node
-    def get(self, node: str):
-        return self._node_dict.get(node, None)
-    def get_node_dict(self):
-        return self._node_dict
-    def set_sorted_nodes(self):
-        result = list()
-        for node in self._node_dict.values():
-            node.set_height()
-            result.append(node)
-        self._sorted_nodes = sorted(result, key=methodcaller('get_height'), reverse=False)
-    def get_sorted_nodes(self):
-        return self._sorted_nodes
-    def __str__(self) -> str:
-        result = ''
-        for node in self.get_sorted_nodes():
-            result += f'\t{node}\n'
-        if not result:
-            return f'{self._name}:\n' + '\tNo Sorted Nodes'
-        return f'{self._name}:\n' + result
-
-
-class DAGNode:
-    def __init__(self, name: str):
-        self._name = name
-        self._direct_prerequisites = set()
-        self._extended_prerequisites = set()
-        self._height = None
-        self._count_direct_prerequisites = None
-
-    def set_direct_prerequisites_from_string_list(self, prerequisites: list, parent_DAG: DAG):
-        for prerequisite in prerequisites:
-            if not parent_DAG.get(prerequisite):
-                parent_DAG.add(DAGNode(prerequisite))
-            self._direct_prerequisites.add(parent_DAG.get(prerequisite))
-            self.set_count_direct_prerequisites()
-
-    def add_direct_prerequisite(self, prerequisite):
-        self._direct_prerequisites.append(DAGNode(prerequisite))
-        self.set_count_direct_prerequisites()
-
-    def get_direct_prerequisites(self):
-        return self._direct_prerequisites
-
-    def get_extended_prerequisites(self):
-        return self._extended_prerequisites
-
-    # def set_name(self, new_name, node_dictionary: dict=None):
-    #     old_name = self._name
-    #     self._name = new_name
-    #     if node_dictionary is not None:
-    #         del(node_dictionary[old_name])
-    #         node_dictionary[new_name] = self
-
-    def get_name(self):
-        return self._name
-
-    def get_height(self):
-        return self._height
-
-    def set_height(self):
-        # print('re-calculate extended prerequisites')
-        # print('re-calculate height')
-        for prerequisite in self._direct_prerequisites:
-            self._extended_prerequisites.add(prerequisite)
-            prerequisite.set_height()
-            self._extended_prerequisites = self._extended_prerequisites | prerequisite.get_extended_prerequisites()
-        self._height = len(self._extended_prerequisites)
-
-    def set_count_direct_prerequisites(self):
-        self._count_direct_prerequisites = len(self._direct_prerequisites)
-    
-    def get_count_direct_prerequisites(self):
-        return self._count_direct_prerequisites
-
-    def __str__(self):
-        formatted_direct_prerequisites = ''
-        for index, prerequisite in enumerate(self._direct_prerequisites):
-            formatted_direct_prerequisites += f'{prerequisite.get_name()}'
-            self.set_count_direct_prerequisites()
-            if index < self._count_direct_prerequisites - 1:
-                formatted_direct_prerequisites += ', '
-        result = f'{self._name} [{self._height}] ({formatted_direct_prerequisites})'
-        return result
-
-
-# Input file is loosely formatted because it was derived from a copy-and-paste of a google doc table
-def parse_google_doc_table_to_class(doc_name: str) -> DAG:
-    dag = DAG('dag')
-    # node_dictionary = dict()
-    current_node_name = None
-    adjacent_node_names = list()
-    with open(doc_name, 'r') as doc_file:
-        for line in doc_file:
-            if not line.strip():
-                if current_node_name and not adjacent_node_names:
-                    dag.add(DAGNode(current_node_name))
-                current_node_name = None
-                adjacent_node_names = list()
-                continue
-            if not current_node_name:
-                current_node_name = line.strip()
-                continue
-            adjacent_node_names.extend(line.strip().split(', '))
-            if not dag.get(current_node_name):
-                dag.add(DAGNode(current_node_name))
-            dag.get(current_node_name).set_direct_prerequisites_from_string_list(adjacent_node_names, dag)
-    dag.set_sorted_nodes()
-    return dag
-
-
 # Input file is loosely formatted because it was derived from a copy-and-paste of a google doc table
 def parse_google_doc_table(doc_name: str):
     matrix = dict()
@@ -148,8 +31,7 @@ def get_all_prerequisites(node: str, matrix: dict) -> set:
             result.add(second_order_prerequisite)
     return result
 
-# Defining "node height" as the total number of prerequisites for a given node
-
+ # Defining "node height" as the total number of prerequisites for a given node
 
 def get_heights(matrix: dict):
     node_heights_dict = dict()
@@ -191,19 +73,10 @@ def print_matrix(matrix: dict):
             result_doc.write('\n')
 
 
+
 def no_dag_unit_test():
-    SOURCE_FILE = 'ParkourClubProjects'
-    FILE_NAME = f'{SOURCE_FILE}/table_doc.txt'
+    FILE_NAME = 'ParkourClubProjects/table_doc.txt'
     adjacency_matrix = parse_google_doc_table(FILE_NAME)
     print_matrix(adjacency_matrix)
 
-
-def yes_dag_unit_test():
-    SOURCE_FILE = 'ParkourClubProjects'
-    FILE_NAME = f'{SOURCE_FILE}/table_doc.txt'
-    test_dag = parse_google_doc_table_to_class(FILE_NAME)
-    # test_dag.set_sorted_nodes() 
-    print(test_dag)
-
-
-yes_dag_unit_test()
+no_dag_unit_test()
